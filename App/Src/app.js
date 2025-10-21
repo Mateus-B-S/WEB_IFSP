ae443b7182d662f7e5004d3c385300cec96c75a1
Mateus Buchalla Santos <mbuchalla23@gmail.com>
2025-10-21 11:11:34 -0300

    comecando_denovo

---
+++ App/Src/app.js
@@ -0,0 +1,27 @@
+const express = require('express');
+
+const app = express();
+
+const methodOverride = require('method-override');
+app.use(methodOverride('_method'))
+
+const path = require('path');
+app.use(express.static(path.join(__dirname, './Front_End/Publico')));
+
+
+// Configurando middlewares
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+
+// Configurando EJS como motor de visualização
+app.set("view engine", "ejs");
+app.set("views", __dirname + "/views");
+
+// Importando e usando as rotas de tarefas e usuarios
+const userRoutes = require('./routes/user_routes');
+app.use('/', userRoutes);
+const tarefasRoutes = require('./routes/tarefas_routes');
+app.use('/paginaInicialTarefas', tarefasRoutes);
+
+module.exports = app;
