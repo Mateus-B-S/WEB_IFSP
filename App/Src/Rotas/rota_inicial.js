d98363e39ead3e3086acb05c9d8ab2aac422c8ed
Mateus Buchalla Santos <mbuchalla23@gmail.com>
2025-12-31 01:12:25 -0300

    Header funcional + ideia de darkmode
    Falta arrumar algumas páginas, fazer funcionar o dropstart/ menu de perfil e, finalmente, adicionar imagens

--- App/Src/Rotas/rota_inicial.js
+++ App/Src/Rotas/rota_inicial.js
@@ -10,6 +10,14 @@ const rotaInicial = (req, res) => {
 const SobreNos = (req, res) => {
     res.render('Sobre/index.ejs');
 };
+const Contato = (req, res) => {
+    res.render('Contato/contato.ejs');
+};
+const Servicos = (req, res) => {
+    res.render('Servicos/servicos.ejs');
+};
 router.get('/', rotaInicial);
 router.get('/SobreNos', SobreNos);
+router.get('/Contato', Contato);
+router.get('/Servicos', Servicos);
 module.exports = router;
