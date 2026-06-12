4c43d40549b31af534bdc28df3fd7b00d26fa975
Mateus Buchalla Santos <mbuchalla23@gmail.com>
2026-06-11 21:09:58 -0300

    aaaaaa

--- App/Src/Controles/animal_controle.js
+++ App/Src/Controles/animal_controle.js
@@ -103,7 +103,7 @@ const deletarAnimal = async (req, res) => {
         res.status(500).render('Avisos/deletarAnimal', { adminSenha: req.session.user.adminSenha, erro: mensagem });
     }
 };
-
+,,,,9699
 
 module.exports = { 
     getAnimalPorId,
